058abfaaecbb7c5cf9924445be34ac407a4c3dde
ChuZZZta <bodzio223@gmail.com>
2018-11-30 14:16:56 +0100

    Refactoring project part3 (moving code to viewmodels)

--- DD2-SbR-Mod/ViewModels/MainViewModel.cs
+++ DD2-SbR-Mod/ViewModels/MainViewModel.cs
@@ -1,14 +1,99 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 
-namespace Sbr.ViewModels
+namespace Sbr
 {
-    class MainViewModel
+    public class MainViewModel
     {
+        public MainViewModel()
+        {
 
+        }
+
+        new List<string> ProcessesList = new List<string>();
+        new List<Map> MapList = new List<Map>();
+        
+        Map[] Maps = new Map[7];
+        public Car[] Cars = new Car[20];
+
+        string JsonDriversPath = "";
+        string DebugBox = "";
+        string SelectedProcess = "";
+
+        bool LapModeActive = false;
+        byte LapLimit = 0;
+        Map SelectedMap;
+
+        private void GetRunningProcesses()
+        {
+            Process[] processes = Process.GetProcesses();
+            processes = processes.Where(x => x.SessionId != 0).ToArray();
+            foreach (Process process in processes)
+            {
+                ProcessesList.Add(process.ProcessName);
+            }
+        }
+
+        private void LoadConfig(object sender, RoutedEventArgs e)
+        {
+            StreamReader sr = new StreamReader(JsonDriversPath);
+            String json = sr.ReadToEnd();
+            Cars = JsonConvert.DeserializeObject<Car[]>(json);
+            foreach (Car car in Cars) car.Processname = SelectedProcess; //SELECTED PROCESS FROM LIST
+            dt.Start();
+        }
+
+       
+        private void GetTrackNames()
+        {
+            //REPLACE THIS STRING IN FINAL RELEASE!!!
+            StreamReader sr = new StreamReader("C:\\Users\\ChuZZZta\\source\\Repos\\Destruction-Derby-2-Scoreboard-Race-Mod\\DD2-SbR-Mod\\Resources\\MapsInfo.json");
+            String json = sr.ReadToEnd();
+            Maps = JsonConvert.DeserializeObject<Map[]>(json);
+            foreach (Map map in Maps)
+            {
+                MapList.Add(map);
+            }
+        }
+
+        private void UpdateScore(object sender, EventArgs e)
+        {
+            foreach (Car car in Cars)
+            {
+                car.Update(LapLimit, LapModeActive, SelectedMap);
+            }
+            if (LapModeActive)
+            {
+                Cars = Cars.OrderByDescending(x => x.lapnumber).ThenBy(x => x.position).ToArray();
+            }
+            else
+            {
+                Cars = Cars.OrderBy(x => x.position).ToArray();
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                DebugBox = DebugBox + " " + (i + 1) + ". " + Cars[i].Number + " " + Cars[i].Name + " ||| LAP: " + Cars[i].lapnumber + "\n";
+            }
+
+
+        }
+        DispatcherTimer dt;
+        private void RefreshScore(object sender, RoutedEventArgs e)
+        {
+            dt = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            dt.Tick += UpdateScore;
+        }
 
 
 
--- DD2-SbR-Mod/Views/MainView.xaml.cs
+++ DD2-SbR-Mod/Views/MainView.xaml.cs
@@ -25,84 +25,13 @@ namespace Sbr
         public MainView()
         {
             InitializeComponent();
-            GetRunningProcesses();
-            GetTrackNames();
         }
-        
-        private void GetRunningProcesses()
-        {
-            Process[] processes = Process.GetProcesses();
-            processes = processes.Where(x => x.SessionId != 0).ToArray();
-            foreach (Process process in processes)
-            {
-                processesList.Items.Add(process.ProcessName);
-            }
-        }
-         
+
         private void SelectJson(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
                 path.Text = openFileDialog.FileName;
-        }
-
-
-
-        //move that code to viewmodels
-        private void LoadConfig(object sender, RoutedEventArgs e)
-        {
-            StreamReader sr = new StreamReader(path.Text);
-            String json = sr.ReadToEnd();
-            cars = JsonConvert.DeserializeObject<Car[]>(json);
-            foreach (Car car in cars) car.Processname = processesList.SelectedItem.ToString();
-            dt.Start();
-        }
-
-
-        Map[] maps = new Map[7];
-        private void GetTrackNames()
-        {
-            //REPLACE THIS STRING IN FINAL RELEASE!!!
-            StreamReader sr = new StreamReader("C:\\Users\\ChuZZZta\\source\\Repos\\Destruction-Derby-2-Scoreboard-Race-Mod\\DD2-SbR-Mod\\Resources\\MapsInfo.json");
-            String json = sr.ReadToEnd();
-            maps = JsonConvert.DeserializeObject<Map[]>(json);
-            foreach (Map map in maps)
-            {
-                maplist.Items.Add(map);
-            }
-        }
-
-        public Car[] cars = new Car[20];
-        private void UpdateScore(object sender, EventArgs e)
-        {
-            debug.Text = "";
-            foreach (Car car in cars)
-            {
-                car.Update(byte.Parse(lapnumber.Text), lapnumbercheck.IsChecked.Value, (Map)maplist.SelectedItem);
-            }
-            if(lapnumbercheck.IsChecked.Value)
-            {
-                cars = cars.OrderByDescending(x=>x.lapnumber).ThenBy(x => x.position).ToArray();
-            }
-            else
-            {
-                cars = cars.OrderBy(x=>x.position).ToArray();
-            }
-            for (int i = 0; i < 20; i++)
-            {
-                debug.Text = debug.Text + " " + (i + 1) + ". " + cars[i].Number + " " + cars[i].Name + " ||| LAP: " + cars[i].lapnumber + "\n";
-            }
-                
-            
-        }
-        DispatcherTimer dt;
-        private void RefreshScore(object sender, RoutedEventArgs e)
-        {
-            dt = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromSeconds(1)
-            };
-            dt.Tick += UpdateScore;
         }
     }
 }
